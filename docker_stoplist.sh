e61722ec9754fcf55a854eacf9f96a34055a4fc4
Zlopi <zlopi.ru@gmail.com>
2024-06-23 15:10:24 +0300

    Create docker_stoplist.sh

---
+++ docker_stoplist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+purge=0
+while IFS=$'\t' read -r id image
+do
+	if grep "^$image:" $0;
+	then
+		docker rm -f "$id"
+		purge="1"
+	fi
+done <<< $(docker ps --format "{{.ID}}\t{{.Image}}")
+
+if [ $purge -eq "1" ] 
+then
+	docker system prune -fa
+fi
+
+exit 0
+
+# stop list images
+olebedev/socks5:latest
+andrey01/softether:4.38-9760-2
